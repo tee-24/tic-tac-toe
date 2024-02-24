eda525718d395620357861a08463cbed30f7bd33
Towbie Idowu <towbieidowu@gmail.com>
2024-02-24 14:58:26 +0000

    Plan

--- run.py
+++ run.py
@@ -2,6 +2,11 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+# Get the player name
+# Display Board
+# Ask for user choice
+# Check for winner
+
 
 
 def get_user_name():
