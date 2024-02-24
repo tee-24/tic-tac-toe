8d599e6b10563a232072454cbcd9779dca909815
Towbie Idowu <towbieidowu@gmail.com>
2024-02-24 14:54:10 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+def get_user_name():
+    """
+    Get the name of the player
+    """
+
+    player = input('What is your name? ')
+    print(f'Hi {player}! Welcome to Tic Tac Toe')
+
+get_user_name()
