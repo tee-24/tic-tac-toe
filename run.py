07ec6f3cc0e3f06992f8610b097a6b9932ca7a24
Towbie Idowu <towbieidowu@gmail.com>
2024-03-07 12:48:28 +0000

    update

--- run.py
+++ run.py
@@ -8,17 +8,22 @@
 # Check for winner
 # Is board full
 
+import random
+
 board_place = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}
 play_game = True
 
 
-def place_marker(board_place, position):
+def place_marker(board_place, position, marker):
     """
-    Place the marker on the board and display the board
+    Place the marker on the board 
+    and display the board
     """
 
     board_place[position] = marker
     display_board(board_place)
+
+    check_winner()
 
 def get_user_name():
     """
@@ -28,6 +33,32 @@ def get_user_name():
     player = input('What is your name? ').capitalize()
     print(f'Hi {player}! Welcome to Tic Tac Toe')
 
+
+# def get_user_marker():
+#     """
+#     Get the markers for Player 1 and Player 2
+#     """
+
+#     choice = ''
+
+#     while choice not in ['X','O']:
+#         choice = input('Would you like to be X or O? ').upper()
+
+#         if choice not in ['X','O']:
+#             print('Invalid choice, please choose X or O')
+
+#     if choice == 'X':
+#         player1 = 'X'
+#         player2 = 'O'
+#         print('Player 1 is X')           
+#         print('Player 2 is O')
+#     else:
+#         player1 = 'O'
+#         player2 = 'X'
+#         print('Player 1 is O')
+#         print('Player 2 is X')
+
+#     return player1, player2 
 
 def get_user_marker():
     """
@@ -42,19 +73,7 @@ def get_user_marker():
         if choice not in ['X','O']:
             print('Invalid choice, please choose X or O')
 
-    if choice == 'X':
-        player1 = 'X'
-        player2 = 'O'
-        print('Player 1 is X')           
-        print('Player 2 is O')
-    else:
-        player1 = 'O'
-        player2 = 'X'
-        print('Player 1 is O')
-        print('Player 2 is X')
-
-    return player1, player2 
-
+    return choice
 
 def display_board(board_place):
     """
@@ -85,22 +104,34 @@ def player_choice():
 
 def computer_choice():
 
-    position = int(input('Where would you like to play? /n'))
-    while position not in range(1,10):
-        print('/nInvalid choice, please choose a number from 1-9')
-        position = int(input('Where would you like to play? '))
+    print("Computer's turn...")
 
-    return position 
+    choice = random.randint(1,9)
+
+    return choice 
 
 def check_winner():
     # Horizontal wins
-    if (board_place[1] == board_place[2] == board_place[3]) or (board_place[4] == board_place[5] == board_place[6]) or (board_place[7] == board_place[8] == board_place[9]):
+    if (
+        board_place[1] == board_place[2] == board_place[3]
+        ) or (
+            board_place[4] == board_place[5] == board_place[6]
+            ) or (
+                board_place[7] == board_place[8] == board_place[9]):
         print('game over')
     # Verical wins
-    elif (board_place[1] == board_place[4] == board_place[7]) or (board_place[2] == board_place[5] == board_place[8]) or (board_place[3] == board_place[6] == board_place[9]):
+    elif (
+        board_place[1] == board_place[4] == board_place[7]
+        ) or (
+            board_place[2] == board_place[5] == board_place[8]
+            ) or (
+                board_place[3] == board_place[6] == board_place[9]):
         print('game over')
     # Diagonal wins
-    elif (board_place[1] == board_place[5] == board_place[9]) or (board_place[3] == board_place[5] == board_place[7]):
+    elif (
+        board_place[1] == board_place[5] == board_place[9]
+        ) or (
+            board_place[3] == board_place[5] == board_place[7]):
         print('game over')
 
 def check_tie():
@@ -111,7 +142,8 @@ def check_tie():
 
 def play_again():
     """
-    Ask if player would like to play again
+    Starts game again if user chooses 'yes'
+    and ends the game if user chooses 'no
     """
 
     answer = input('Do you want to play again? ').capitalize()
@@ -120,20 +152,43 @@ def play_again():
         answer = input('Please type Yes or No: ').capitalize()
 
     if answer in ['Y', 'Yes']:
-        print('Yay!')
+        print("Great, let's play again!")
     else:
         print('Thanks for playing!')
+        play_game = False
 
     return answer 
 
-while play_game == True:
+def run_game():
 
     get_user_name()
 
-    marker = get_user_marker()
+    user_marker = get_user_marker()
+
+    if user_marker == 'X':
+        computer_marker = 'O'
+    else:
+        computer_marker = 'X'
+
+
+while play_game == True:
+
+    user_marker = get_user_marker()
+
+    if user_marker == 'X':
+        computer_marker = 'O'
+    else:
+        computer_marker = 'X'
 
     display_board(board_place)
 
-    player_choice()
+    player_choice = player_choice()
 
-    place_marker(board_place, player_choice())
+    place_marker(board_place, player_choice, user_marker)
+
+    computer_choice = computer_choice()
+
+    place_marker(board_place, computer_choice, computer_marker)
+
+    play_again()
+
