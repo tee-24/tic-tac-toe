59fa5bf450fa1312dbbe041eab31ce592ba22125
Towbie Idowu <towbieidowu@gmail.com>
2024-03-07 14:19:06 +0000

    update

--- run.py
+++ run.py
@@ -8,10 +8,13 @@
 # Check for winner
 # Is board full
 
+import emoji
 import random
+
 
 board_place = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}
 play_game = True
+turn = 0
 
 
 def place_marker(board_place, position, marker):
@@ -19,7 +22,6 @@ def place_marker(board_place, position, marker):
     Place the marker on the board 
     and display the board
     """
-
     board_place[position] = marker
     display_board(board_place)
 
@@ -29,9 +31,17 @@ def get_user_name():
     """
     Get the name of the player
     """
+    player = ''
 
-    player = input('What is your name? ').capitalize()
-    print(f'Hi {player}! Welcome to Tic Tac Toe')
+    while len(player) < 3 or not player.isalpha():
+        player = input('\nWhat is your name? ').capitalize()
+
+        if player.isalpha() == False:
+            print('Name can only contain letters, please try again')
+        elif len(player) < 3:
+            print('Name must be a minimum of 3 characters, please try again')
+
+    print(emoji.emojize(f'Hi {player}! Welcome to Tic Tac Toe :grinning_face_with_big_eyes:\n'))
 
 
 # def get_user_marker():
@@ -64,14 +74,13 @@ def get_user_marker():
     """
     Get the markers for Player 1 and Player 2
     """
-
     choice = ''
 
     while choice not in ['X','O']:
-        choice = input('Would you like to be X or O? ').upper()
+        choice = input('Would you like to be X or O? \n').upper()
 
         if choice not in ['X','O']:
-            print('Invalid choice, please choose X or O')
+            print('Invalid choice, please choose X or O\n')
 
     return choice
 
@@ -79,7 +88,6 @@ def display_board(board_place):
     """
     Print out the game board
     """
-
     print(f' {board_place[1]} | {board_place[2]} | {board_place[3]}')
     print(f' {board_place[4]} | {board_place[5]} | {board_place[6]}')
     print(f' {board_place[7]} | {board_place[8]} | {board_place[9]}')
@@ -90,7 +98,11 @@ def player_choice():
     position = ''
 
     while position not in board_place or board_place[position] in ['X', 'O']:
-        position = int(input('Where would you like to play? '))
+        try:
+            position = int(input('Where would you like to play? '))
+        except ValueError:
+            print('Invalid choice, please choose a number from 1-9')
+            continue
 
         if position not in board_place:
             print('Invalid choice, please choose a number from 1-9')
@@ -159,6 +171,15 @@ def play_again():
 
     return answer 
 
+def swap_turn(turn):
+    
+    if turn % 2 == 0:
+        return user_marker
+    else:
+        return computer_marker
+    
+    turn+=1
+
 def run_game():
 
     get_user_name()
@@ -173,6 +194,8 @@ def run_game():
 
 while play_game == True:
 
+    get_user_name()
+
     user_marker = get_user_marker()
 
     if user_marker == 'X':
@@ -183,12 +206,10 @@ while play_game == True:
     display_board(board_place)
 
     player_choice = player_choice()
-
-    place_marker(board_place, player_choice, user_marker)
-
     computer_choice = computer_choice()
+    marker = swap_turn(turn)
 
-    place_marker(board_place, computer_choice, computer_marker)
+    place_marker(board_place, computer_choice, marker)
 
     play_again()
 
